b5aaa5e7cf96dabf0acd59edc243ffc5bf3d6b5b
Linh Thai <thaitruclinh151296@gmail.com>
2017-11-25 18:34:27 +0700

    Upload code 2

---
+++ BinhDuongVirtualTour/BinhDuongVirtualTour/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinhDuongVirtualTour.Models;
+namespace BinhDuongVirtualTour.Controllers
+{
+    public class HomeController : Controller
+    {
+        BinhDuongVirtualTourEntities db = new BinhDuongVirtualTourEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _VisitImagePartial()
+        {
+            var ha = db.VISIT_IMAGE.ToList();
+            return PartialView("~/Views/Home/_VisitImagePartial.cshtml", ha);
+        }
+    }
+}
---
+++ BinhDuongVirtualTour/BinhDuongVirtualTour/Controllers/VisitController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinhDuongVirtualTour.Models;
+namespace BinhDuongVirtualTour.Controllers
+{
+    public class VisitController : Controller
+    {
+       BinhDuongVirtualTourEntities db = new BinhDuongVirtualTourEntities();
+        // GET: Visit
+        public ActionResult _MenuPartial()
+        {
+
+            var da = db.VISIT_IMAGE.ToList();
+            return PartialView("_MenuPartial", da);
+        }
+        public ActionResult Visit(int imgId)
+        {
+
+            var da = db.VISIT_IMAGE.SingleOrDefault(n => n.Img_Id == imgId);
+                Session["idType"] = db.VISIT_IMAGE.SingleOrDefault(n => n.Img_Id == imgId).idType.ToString();
+            Session["lat"] = db.VISIT_IMAGE.SingleOrDefault(n => n.Img_Id == imgId).lat.ToString();
+            Session["lng"] = db.VISIT_IMAGE.SingleOrDefault(n => n.Img_Id == imgId).lng.ToString();
+            return View(da);
+        }
+    }
+}
